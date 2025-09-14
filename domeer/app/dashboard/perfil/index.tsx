af5d5f213b52f4246c51eec39e02f7e5563ea75c
stephanie-rondon <stephanie.rondon@escola.pr.gov.br>
2025-09-14 19:28:24 -0300

    Criação das pastas do perfil, metas e do calendario

--- domeer/app/dashboard/perfil/index.tsx
+++ domeer/app/dashboard/perfil/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Login() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Página de Login</Text>
+      <Text>Página de Perfil</Text>
     </View>
   );
 }
