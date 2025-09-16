ed3b84c6d8349ba10f77c2d3853ac921a3aae038
stephanie-rondon <stephanie.rondon@escola.pr.gov.br>
2025-09-16 10:49:49 -0300

    Adiçao de pastas do dashboard

--- domeer/app/dashboard/calendario/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Login() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Página de Calendário</Text>
-    </View>
-  );
-}
--- domeer/app/dashboard/perfil/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Login() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Página de Perfil</Text>
-    </View>
-  );
-}
