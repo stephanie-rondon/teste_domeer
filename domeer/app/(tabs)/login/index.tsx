15ebed4ce14a4bbf9ba1e07d4e0cf507181a93c9
stephanie-rondon <stephanie.rondon@escola.pr.gov.br>
2025-09-16 22:36:20 -0300

    Adicionei a primeira versão da página de login

--- domeer/app/(tabs)/login/index.tsx
+++ domeer/app/(tabs)/login/index.tsx
@@ -1,9 +1,57 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, TextInput, StyleSheet, Pressable, Text } from "react-native";
 
 export default function Login() {
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const handleLogin = () =>{
+    console.log("Name:", name);
+    console.log("Password:", password);
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Página de Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Página de Login</Text>
+      <TextInput style={styles.input} placeholder="Nome do usuário" onChangeText={setName} value={name}/>
+      <TextInput style={styles.input} placeholder="Senha" onChangeText={setPassword} value={password}/>
+      <Pressable style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </Pressable>
     </View>
+    
   );
 }
+
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#145",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 22,
+    color: "#fff",
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#fff",
+    width: 200, 
+    height: 40,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10, 
+  },
+  button: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#145",
+  }
+})
